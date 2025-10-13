e245e2177999069482a3af200a513f155b27943d
maxxizup <maksimkaxd1@gmail.com>
2025-10-13 15:13:30 +1000

    added react-router-dom, routes and realized lazy loading chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
+import { Link, Route, Routes } from "react-router-dom";
 import { Counter } from "./components/Counter";
 import './index.scss'
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
 
 const App = () => {
   return (
     <div className='App'>
-        asdsadasd
-        <Counter />
+        <Link to={'/about'}>About</Link>
+        <Link to={'/'}>Main</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+                <Route path={'/about'} element={<AboutPageAsync />} />
+                <Route path={'/'} element={<MainPageAsync />} />
+            </Routes>
+        </Suspense>
     </div>
   )
 }
