73a97776c0475832a23d6f77fb72832476b07ec3
maxxizup <maksimkaxd1@gmail.com>
2026-01-16 16:41:38 +1000

    added LangSwitcher and i18next lib

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,12 +1,31 @@
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import webpack from 'webpack';
-import { BuildOptions } from './types/config';
+import {BuildOptions} from './types/config';
 
-export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
-    
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
+
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
+    }
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                "plugins": [
+                    ["i18next-extract",
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
     }
 
     const cssLoader = {
@@ -29,7 +48,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
             "sass-loader",
         ],
     }
-    
+
     // Если не используем TS - нужен babel-loader, который транспилирует стандарт js в старый,
     // чтобы он поддерживался старыми браузерами
     const typescriptLoader = {
@@ -51,6 +70,7 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     return [
         fileLoader,
         svgLoader,
+        babelLoader,
         typescriptLoader,
         cssLoader,
     ]
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,23 @@
 import './styles/index.scss'
-import { classNames } from "shared/lib/classNames/classNames";
-import { useTheme } from "app/providers/ThemeProvider"
-import { AppRouter } from "app/providers/router"
-import { Navbar } from "widgets/Navbar";
-import { Sidebar } from "widgets/Sidebar";
-
+import {classNames} from "shared/lib/classNames/classNames";
+import {useTheme} from "app/providers/ThemeProvider"
+import {AppRouter} from "app/providers/router"
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
+import {Suspense} from "react";
 
 const App = () => {
-    const { theme } = useTheme();
+    const {theme} = useTheme();
 
     return (
         <div className={classNames('App', {}, [theme])}>
-            <Navbar />
-            <div className="content-page">
-                <Sidebar />
-                <AppRouter />
-            </div>
+            <Suspense fallback="">
+                <Navbar/>
+                <div className="content-page">
+                    <Sidebar/>
+                    <AppRouter/>
+                </div>
+            </Suspense>
         </div>
     )
 }
