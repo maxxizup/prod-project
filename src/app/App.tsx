4e80db2c00615ff104c818a39dd0c3be7802531d
maxxizup <maksimkaxd1@gmail.com>
2026-07-26 21:10:10 +1000

    refactored modal theme

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
     const { theme } = useTheme();
 
     return (
-        <div className={classNames('App', {}, [theme])}>
+        <div className={classNames('App', {}, [])}>
             <Suspense fallback="">
                 <Navbar />
                 <div className="content-page">
